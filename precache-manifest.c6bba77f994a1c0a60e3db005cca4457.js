ce0243826102de471195e2e1aaa87e33147ca0cc
Akshaytyag <you@example.com>
2020-09-28 19:34:31 +0530

    Updates

---
+++ precache-manifest.c6bba77f994a1c0a60e3db005cca4457.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b50b3fbe8adfefb63f400526333188a",
+    "url": "/Demo/index.html"
+  },
+  {
+    "revision": "83294863b08706bbf838",
+    "url": "/Demo/static/css/2.e9860b44.chunk.css"
+  },
+  {
+    "revision": "0e8952c823897726f2e9",
+    "url": "/Demo/static/css/main.f2dcfa35.chunk.css"
+  },
+  {
+    "revision": "83294863b08706bbf838",
+    "url": "/Demo/static/js/2.623abeb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Demo/static/js/2.623abeb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8952c823897726f2e9",
+    "url": "/Demo/static/js/main.e465e2c5.chunk.js"
+  },
+  {
+    "revision": "1b3bc9000159a1a724eb",
+    "url": "/Demo/static/js/runtime-main.90c34fa7.js"
+  }
+]);
